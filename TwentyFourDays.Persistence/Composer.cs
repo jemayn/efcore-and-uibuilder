85aa4e60b3de01a62a944417aae8a549abcd3206
Jesper Mayntzhusen <jm@ecreo.dk>
2023-11-08 12:53:27 +0100

    add persistence project

---
+++ TwentyFourDays.Persistence/Composer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TwentyFourDays.Persistence.Services;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace TwentyFourDays.Persistence;
+
+public class Composer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.AddTransient<MoviesImportService>();
+    }
+}
---
+++ TwentyFourDays.Persistence/Services/MoviesImportService.cs
@@ -0,0 +1,14 @@
+﻿namespace TwentyFourDays.Persistence.Services;
+
+public class MoviesImportService
+{
+    public MoviesImportService()
+    {
+        
+    }
+
+    public async Task Import()
+    {
+        
+    }
+}
